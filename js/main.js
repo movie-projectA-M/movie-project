d673763494091fa3524f536830da5563211261dd
Micah Nelthropp <micahnelthropp@gmail.com>
2023-12-01 16:06:36 -0600

    javascript

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const getMovies = async () => {
+    const url = "http://localhost:3000/movies";
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+};
+const getMovie = async (id) => {
+    const url = `  http://localhost:3000/movies${id}`;
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+};
+
+const deleteMovie = async (id) => {
+    const url = `  http://localhost:3000/movies/${id}`;
+    const options = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+};
+export const postMovie = async ({ title,rating }) => {
+    const newBook = {
+        title,
+        rating,
+    };
+    const body = JSON.stringify(newBook);
+
+    const url = `  http://localhost:3000/movies`;
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: body,
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+};
+//MAIN
+(async ()=>{
+
+})();
